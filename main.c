f4046f002c44de3de320fae11b0f8ace53e87bcb
Wyatt Jia <i@eventloop.live>
2022-10-25 15:20:50 +0800

    Remove useless func call.
    Signed-off-by: Wyatt Jia <i@eventloop.live>

--- main.c
+++ main.c
@@ -1,26 +1,8 @@
 #include <sys/wait.h>
-waitpid() and associated macros
 #include <unistd.h>
-chdir()
-fork()
-exec()
-pid_t
 #include <stdlib.h>
-malloc()
-realloc()
-free()
-exit()
-execvp()
-EXIT_SUCCESS, EXIT_FAILURE
 #include <stdio.h>
-fprintf()
-printf()
-stderr
-getchar()
-perror()
 #include <string.h>
-strcmp()
-strtok()
 
 
 int lsh_execute(char **args)
